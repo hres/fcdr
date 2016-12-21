5458c2061638374060db12e350920cf671a95385
Romario Nelson <nelsonromario@hotmail.com>
2016-12-21 14:54:57 -0500

    comit

--- view_package.php
+++ view_package.php
@@ -2,11 +2,11 @@
 
 <?php
 
-$PID = ($_GET['ProductID']?$_GET['ProductID']:'');
+$ProductID = ($_GET['ProductID']?$_GET['ProductID']:'');
 
 
 	
-	$query = "  SELECT * FROM Package where ProductIDP = $PID";
+	$query = "  SELECT * FROM Package where ProductIDP = $ProductID";
 	$result = mysqli_query($conn,$query);
 
 
@@ -14,7 +14,7 @@ if (!$result) {
     echo "ERRORS";
 }
 	 while($row = $result->fetch_assoc()) {
-		 echo "<tr><td> <a href=package_details.php?PackageID=" .$row['PackageID'] . ">". $row['Label_UPC'] . "</a></td><td>" . $row['Label_Description'] . "</td><td>" . $row['Collection_Date'] . "</td><td>" . $row['Source'] . "</td><td>" . $row['Create_Date'] . "</td><td>" . $row['Last_Edit_Date'] . "</td><td>" . $row['Last_Edited_By'] . "</td></tr>";                       
+		 echo "<tr><td> <a href=package_details.php?ProductID=$ProductID&PackageID=" .$row['PackageID'] . ">". $row['Label_UPC'] . "</a></td><td>" . $row['Label_Description'] . "</td><td>" . $row['Collection_Date'] . "</td><td>" . $row['Source'] . "</td><td>" . $row['Create_Date'] . "</td><td>" . $row['Last_Edit_Date'] . "</td><td>" . $row['Last_Edited_By'] . "</td></tr>";                       
 		
 	 }
 	
