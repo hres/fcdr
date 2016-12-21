fd850e0b7bdaa784f3db51000ce553193292896b
Romario Nelson <nelsonromario@hotmail.com>
2016-12-21 12:02:37 -0500

    comit

--- new-market.php
+++ new-market.php
@@ -11,28 +11,6 @@ if (isset($_POST['search'])) {
 /* 	error_reporting(E_ALL);
 	ini_set('display_errors', 1); */
 
-	
-	$Sales_UPC                       = $_POST['Sales_UPC'];
-	$Dollar_Volume                   = $_POST['Dollar_Volume'];
-	$kilo_vol                        = $_POST['Kilo_Vol'];
-	$Sales_Year                      = $_POST['Sales_Year'];
-	$Nielsen_Category                = $_POST['Nielsen_Category'];
-	$Source                          = $_POST['Source'];
-	$Sales_Description               = $_POST['Sales_Description'];
-	$Comments                        = $_POST['Comments'];
-	$Collection_Date                 = $_POST['Collection_Date'];
-	$Brand                           = $_POST['Brand'];
-	$Dollar_Rank                     = $_POST['Dollar_Rank'];
-	$Dollar_Volume_PerCentage_Change = $_POST['Dollar_Volume_PerCentage_Change'];
-	$Kilo_Volume_Percent_Change      = $_POST['Kilo_Volume_Percent_Change'];
-	$Average_AC_Dist                 = $_POST['Average_AC_Dist'];
-	$Average_Retail_Price            = $_POST['Average_Retail_Price'];
-	$Dollar_Volume_Total             = $_POST['Dollar_Volume_Total'];
-	$Kilo_Volume_Total               = $_POST['Kilo_Volume_Total'];
-	$Control_Label_Flag              = $_POST['Control_Label_Flag'];
-	$Dollar_Share                    = $_POST['Dollar_Share'];
-	$Kilo_Share                      = $_POST['Kilo_Share'];
-	$Cluster_Number                  = $_POST['Cluster_Number'];
 
 /* 	$query2 =<<<EOQ
 SELECT *
@@ -53,27 +31,27 @@ EOQ;
 
 		$param = array(
 			$productID,
-			$Sales_UPC,
-			$Dollar_Volume,
-			$kilo_vol,
-			$Sales_Year,
-			$Nielsen_Category,
-			$Source,
-			$Sales_Description,
-			$Comments,
-			$Collection_Date,
-			$Brand,
-			$Dollar_Rank,
-			$Dollar_Volume_PerCentage_Change,
-			$Kilo_Volume_Percent_Change,			
-			$Kilo_Share,
-			$Average_AC_Dist,
-			$Average_Retail_Price,
-			$Dollar_Volume_Total,
-			$Kilo_Volume_Total,
-			$Control_Label_Flag,
-			$Dollar_Share, 
-			$Cluster_Number
+			$_POST['Sales_UPC'],
+			$_POST['Dollar_Volume'],
+			$_POST['Kilo_Vol'],
+			$_POST['Sales_Year'],
+			$_POST['Nielsen_Category'],
+			$_POST['Source',
+			$_POST['Sales_Description'],
+			$_POST['Comments'],
+			$_POST['Collection_Date'],
+			$_POST['Brand'],
+			$_POST['Dollar_Rank'],
+			$_POST['Dollar_Volume_PerCentage_Change'],
+			$_POST['Kilo_Volume_Percent_Change'],			
+			$_POST['Kilo_Share'],
+			$_POST['Average_AC_Dist'],
+			$_POST['Average_Retail_Price'],
+			$_POST['Dollar_Volume_Total'],
+			$_POST['Kilo_Volume_Total'],
+			$_POST['Control_Label_Flag'],
+			$_POST['Dollar_Share'], 
+			$_POST['Cluster_Number']
 		);
 
 		$query =<<<EOQ
@@ -102,12 +80,12 @@ INSERT INTO Sales (
        Dollar_Share,
        Cluster_Number
 )
-VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 EOQ;
 
 	
 		$stmt = $conn->prepare($query);
-		$stmt->bind_param("iiddissssssddddddddsdi", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20]);
+		$stmt->bind_param("iiddissssssddddddddsdi", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21]);
 		$result = $stmt->execute();
 
 print $stmt->error; 
