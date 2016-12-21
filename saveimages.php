3f00f6ce64495cc367d653077dca05a25bdd4ebf
Romario Nelson <nelsonromario@hotmail.com>
2016-12-21 13:17:20 -0500

    comit

--- saveimages.php
+++ saveimages.php
@@ -5,7 +5,7 @@
 error_reporting(0);   
 $dir=substr(uniqid(),-7);
   $valid_formats = array("jpg", "png", "gif", "jpeg");
-$max_file_size = 1024*10000; //1000 kb
+$max_file_size = 	10240*10000; //10000 kb
   define ("MAX_SIZE","2000");
 /* $path = "Prototype/uploads/"; // Upload directory
 mkdir ($path, 0744); */
