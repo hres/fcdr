cea06a064b2a11a254a4c6a063f872ae5c8dacb5
Romario Nelson <nelsonromario@hotmail.com>
2016-12-21 09:00:35 -0500

    comit

--- cleardb.php
+++ cleardb.php
@@ -2,11 +2,11 @@
 <?php
 if (isset($_POST['clearbd'])) {
 							
-	$query1 =  "Delete from Product_Component where ComponentID > 0"; 
-	$result = mysqli_query($conn,$query1);	
+
 	$query2 =  "Delete from Sales where SalesID > 0"; 	
 	$result = mysqli_query($conn,$query2);	
 	
+
 							
 }
 
