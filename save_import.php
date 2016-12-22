64f308af4d1ac4c39f80c19bdf697df3f90e92ce
Romario Nelson <nelsonromario@hotmail.com>
2016-12-22 10:22:45 -0500

    comit

--- save_import.php
+++ save_import.php
@@ -407,10 +407,7 @@ EOQ;
 							$Product_Description,
 							$Classification_Number,
 							$Classification_Type,
-							$Comments
-							
-							
-							
+							$Comments				
 						); 
 
 
@@ -450,7 +447,7 @@ EOQ;
 							$stmt = $conn->prepare($insert_query2);
 						$stmt->bind_param("iisssdddddddddsssisdddisdss", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[22], $param[23], $param[24], $param[25], $param[26]);
 							$result_insert = $stmt->execute();
-
+							print $stmt->error_list;
 							if (!$result_insert) {
 								echo "error";
 							}
