b4ef0ec5f8971bf3c458364dc0a8f0293d755fc8
Romario Nelson <nelsonromario@hotmail.com>
2016-12-28 13:06:11 -0500

    refactored Save label

--- save-label-updated.php
+++ save-label-updated.php
@@ -235,7 +235,7 @@ EOQ;
 
 							}
 
-		echo "<script type=\"text/javascript\"> document.getElementById (\"confirm-message\"). innerHTML = \"<h3 style=\"color:green;\">Label Successfully updated. Redirecting to the previous page...</h3>\";</script>"
+		echo "<script type=\"text/javascript\"> document.getElementById (\"confirm-message\"). innerHTML = \"<h3 style=\"color:green;\">Label Successfully updated. Redirecting to the previous page...</h3>\";</script>";
 		echo "<script>setTimeout(\"location.href = 'edit_package.php?PackageID=$packageID';\",200);</script>";
 
 		}
