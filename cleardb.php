fd22f545e6533c84d55bb8b66590bab82b648821
Romario Nelson <nelsonromario@hotmail.com>
2016-12-28 15:21:04 -0500

    confirm message product

--- cleardb.php
+++ cleardb.php
@@ -9,8 +9,10 @@
  */
 	$query2 =  "Delete from $dbname.Sales where SalesID > 0"; 	
 	$result = mysqli_query($conn,$query2);	
-	
-
+	Product_Grouping
+	querychange = "ALTER TABLE Sales MODIFY Product_Grouping DECIMAL";
+	$result_querychange = mysqli_query($conn,$querychange);	
+	if(!result_querychange){echo "ERROR";}
 	
 		$query3 =  "Delete from $dbname.Product_Component where ComponentID > 0"; 	
 		$result3 = mysqli_query($conn,$query3);	
--- save_import.php
+++ save_import.php
@@ -7,8 +7,7 @@ require_once "Classes/PHPExcel.php";
 
 if (isset($_POST['search'])) {
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
 ini_set('memory_limit', '256M');
 	if (is_uploaded_file($_FILES['file_save']['tmp_name'])) {
 		echo "<h3>" . "File ". $_FILES['file_save']['name'] ." uploaded successfully." . "</h3>";
@@ -190,7 +189,7 @@ SELECT DISTINCT ProductIDS, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
 EOQ;
 
 						$stmt = $conn->prepare($insert_query);
-						$stmt->bind_param("ssssdddddddddsssisdddisdssi", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[22], $param[23], $param[24], $param[25], $Sales_UPC);
+						$stmt->bind_param("ssssdddddddddsssisddddsdssi", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[22], $param[23], $param[24], $param[25], $Sales_UPC);
 						$result_insert = $stmt->execute();
 			 
 						if ($Product_Description != null) {
@@ -223,7 +222,7 @@ SELECT DISTINCT SalesID
 EOQ;
 
 						$stmt = $conn->prepare($grouping);
-						$stmt->bind_param("i", $Product_Grouping);
+						$stmt->bind_param("d", $Product_Grouping);
 						$check_grouping_result = $stmt->execute();
 						$stmt->store_result();
 
@@ -298,7 +297,7 @@ SELECT DISTINCT ProductIDS, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
 EOQ;
 
 							$stmt = $conn->prepare($insert_query);
-						$stmt->bind_param("ssssdddddddddsssisdddisdssi", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[22], $param[23], $param[24], $param[25], $Product_Grouping);
+						$stmt->bind_param("ssssdddddddddsssisddddsdssi", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[22], $param[23], $param[24], $param[25], $Product_Grouping);
 							$result_insert = $stmt->execute();
 
 							$input4 = "Record : $Record, $Sales_Description";
@@ -417,7 +416,7 @@ VALUES ( NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
 EOQ;
 
 							$stmt = $conn->prepare($insert_query2);
-							$stmt->bind_param("issssdddddddddsssisdddis", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[22], $param[26]);
+							$stmt->bind_param("issssdddddddddsssisdddds", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[22], $param[26]);
 							$result_insert = $stmt->execute();
 							print $stmt->error_list;
 							if (!$result_insert) {
@@ -574,7 +573,7 @@ EOQ;
 
 
 							$stmt = $conn->prepare($insert_query);
-						$stmt->bind_param("ssssdddddddddsssisdddiss", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[25], $Sales_UPC);
+						$stmt->bind_param("ssssdddddddddsssisddddss", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[25], $Sales_UPC);
 							$result_insert = $stmt->execute();
 
 							$input7 = "Record : $Record, $Sales_Description";
@@ -660,7 +659,7 @@ EOQ;
 
 
 								$stmt = $conn->prepare($insert_queryt);
-							$stmt->bind_param("sssssdddddddddsssisdddis", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[22], $param[26]);
+							$stmt->bind_param("sssssdddddddddsssisdddds", $param[0], $param[1], $param[2], $param[3], $param[4], $param[5], $param[6], $param[7], $param[8], $param[9], $param[10], $param[11], $param[12], $param[13], $param[14], $param[15], $param[16], $param[17], $param[18], $param[19], $param[20], $param[21], $param[22], $param[26]);
 								$result_insertt = $stmt->execute();
 	
 
