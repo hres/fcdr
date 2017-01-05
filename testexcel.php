ec9d0ee4428a7d13f8edd713b246bc2d9a8fd605
Romario Nelson <nelsonromario@hotmail.com>
2017-01-05 13:02:08 -0500

    created test file please ignore

--- testexcel.php
+++ testexcel.php
@@ -3,7 +3,7 @@ require_once 'Classes/PHPExcel.php';
 $objPHPExcel = new PHPExcel();
 $objPHPExcel->getActiveSheet()->setCellValue('A1', 'hello world!');
 $objPHPExcel->getActiveSheet()->setTitle('Chesse1');
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-type: application/vnd.ms-excel');
 header('Content-Disposition: attachment;filename="helloworld.xlsx"');
 header('Cache-Control: max-age=0');
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
