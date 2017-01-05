45125c5aac47f4e3fa3f4bfc153b03cc310dc5c5
Romario Nelson <nelsonromario@hotmail.com>
2017-01-05 13:04:26 -0500

    created test file please ignore

--- testexcel.php
+++ testexcel.php
@@ -6,6 +6,6 @@ $objPHPExcel->getActiveSheet()->setTitle('Chesse1');
 header('Content-type: application/vnd.ms-excel');
 header('Content-Disposition: attachment;filename="helloworld.xlsx"');
 header('Cache-Control: max-age=0');
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2010');
 $objWriter->save('php://output');
 ?>
