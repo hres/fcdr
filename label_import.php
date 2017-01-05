f9ff803fd5553cac9e51149a6210cdbc9736ddf5
Romario Nelson <nelsonromario@hotmail.com>
2017-01-05 13:14:04 -0500

    created test file please ignore

--- label_import.php
+++ label_import.php
@@ -2,6 +2,14 @@
 
 <?php
 		
+$objPHPExcel = new PHPExcel();
+$objPHPExcel->getActiveSheet()->setCellValue('A1', 'hello world!');
+$objPHPExcel->getActiveSheet()->setTitle('Chesse1');
+header('Content-type: application/vnd.ms-excel');
+header('Content-Disposition: attachment;filename="helloworld"');
+header('Cache-Control: max-age=0');
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save('php://output');
 
 	error_reporting(E_ALL);
 ini_set('display_errors', 1);
--- testexcel.php
+++ testexcel.php
@@ -4,7 +4,7 @@ $objPHPExcel = new PHPExcel();
 $objPHPExcel->getActiveSheet()->setCellValue('A1', 'hello world!');
 $objPHPExcel->getActiveSheet()->setTitle('Chesse1');
 header('Content-type: application/vnd.ms-excel');
-header('Content-Disposition: attachment;filename="helloworld.xlsx"');
+header('Content-Disposition: attachment;filename="helloworld"');
 header('Cache-Control: max-age=0');
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 $objWriter->save('php://output');
