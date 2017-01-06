254d2e511bb596e3308b785b460c82f9f34d6d73
Romario Nelson <nelsonromario@hotmail.com>
2017-01-06 09:15:18 -0500

    test download

--- testexcel.php
+++ testexcel.php
@@ -1,11 +1,10 @@
 <?php
-require_once 'Classes/PHPExcel.php';
-$objPHPExcel = new PHPExcel();
-$objPHPExcel->getActiveSheet()->setCellValue('A1', 'hello world!');
-$objPHPExcel->getActiveSheet()->setTitle('Chesse1');
-header('Content-type: application/vnd.ms-excel');
-header('Content-Disposition: attachment;filename="helloworld"');
+header('Content-Type: 'application/vnd.ms-excel');
+header('Content-Disposition: attachment;filename="test.xlsx"');
 header('Cache-Control: max-age=0');
+
+
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+ob_end_clean();
+ob_start();
 $objWriter->save('php://output');
-?>
