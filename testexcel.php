6e11c10d074275b0c99eb3f3fe6c5959a1989b9e
Romario Nelson <nelsonromario@hotmail.com>
2017-01-06 11:55:02 -0500

    test download

--- testexcel.php
+++ testexcel.php
@@ -6,7 +6,6 @@ $filename = "Webinfopen.xls"; // File Name
 // Download file
 header("Content-Disposition: attachment; filename=\"$filename\"");
 header("Content-Type: application/vnd.ms-excel");
-$user_query = mysql_query('select name,work from info');
 // Write data to file
 $row = 0;
 while ($row <10) {
