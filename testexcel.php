1a58806f1640a07dcfe6e516e7b089bfb433f717
Romario Nelson <nelsonromario@hotmail.com>
2017-01-09 08:40:47 -0500

    test download

--- testexcel.php
+++ testexcel.php
@@ -9,9 +9,15 @@ header("Content-Disposition: attachment; filename=\"$filename\"");
 header("Content-Type: application/vnd.ms-excel");
 // Write data to file
 $row = 0;
-while ($row <10) {
-
-    echo "Romy";
-	++$row;
-}
+$table = '<table>';
+$table .= '<tr>';
+$table .= '<td>1st row, 1st cell</td>';
+$table .= '<td>1st row, 2nd cell</td>';
+$table .= '</tr>';
+$table .= '<tr>';
+$table .= '<td>2nd row, 1st cell</td>';
+$table .= '<td>2nd row, 2nd cell</td>';
+$table .= '</tr>';
+$table .= '</table>';
+echo $table;
 ?>
