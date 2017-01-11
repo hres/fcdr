c013615e10492e3f0212242c815688285a84c837
Romario Nelson <nelsonromario@hotmail.com>
2017-01-11 09:46:00 -0500

    refactoring MS import

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 	<title>FCDR</title>
 	<link rel="shortcut icon" type="image/png" href="/media/images/favicon.png">
 
-	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 
 <link rel="stylesheet" href="./theme-gcwu-fegc/css/theme.min.css">
