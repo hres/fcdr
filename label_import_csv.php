50acaf0b79796f8d4225f29ce19d9efd6de7dc54
Romario Nelson <nelsonromario@hotmail.com>
2017-01-16 08:05:36 -0500

    comitted

--- label_import_csv.php
+++ label_import_csv.php
@@ -569,9 +569,38 @@ EOQ;
 
 						$stmt = $conn->prepare($query1);
 						$stmt->bind_param("sssssssssssssssssssssssssssssi", $params[0], $params[1], $params[2], $params[3], $params[4], $params[5], $params[6], $params[7], $params[8], $params[9], $params[10], $params[11], $params[12], $params[13], $params[14], $params[15], $params[16], $params[17], $params[18], $params[19], $params[20], $params[21], $params[22], $params[23], $params[24], $params[25], $params[26], $params[27], $params[28], $Neilsen_Item_Rank_UPC);
-						$nid = mysqli_insert_id($conn);
 						$stmt->execute();
+						$nid = mysqli_insert_id($conn);
 
+						$params = array($Brand, $Manufacturer, $Neilsen_Item_Rank_UPC);
+						if ($Product_Description != null) {
+							$params = array($Product_Description) + $params;
+							$query_update =<<<EOQ
+UPDATE Product
+   SET Description = ?, Brand = ?, Manufacturer = ?
+ WHERE ProductID = (
+          SELECT DISTINCT ProductIDS
+            FROM Sales
+           WHERE Sales_UPC = ?
+       )
+EOQ;
+							$stmt = $conn->prepare($query_update);
+							$stmt->bind_param("sssi", $params[0], $params[1], $params[2], $params[3]);
+						} else {
+							$query_update =<<<EOQ
+UPDATE Product
+   SET Brand = ?, Manufacturer = ?
+ WHERE ProductID = (
+          SELECT DISTINCT ProductIDS
+            FROM Sales
+           WHERE Sales_UPC = ?
+       )
+EOQ;
+							$stmt = $conn->prepare($query_update);
+							$stmt->bind_param("ssi", $params[0], $params[1], $params[2]);
+						
+							$stmt->execute();
+}
 
 
 $query_insertx =<<<EOQ
@@ -600,6 +629,9 @@ for ($row2 = 0; $row2 < 94; $row2++) {
 								$stmt->bind_param("iidsds",$nid, $cars[$row2][0],$cars[$row2][1],$cars[$row2][2],$cars[$row2][3],$cars[$row2][4]);
 								$results = $stmt->execute();
 }
+
+
+
 
 						$input3= "Record : $Record, $Label_Description";
 						$linked_sales->push($input3);
