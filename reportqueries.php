9abe445293d5a5084730b4c19ada890c3b66ed9e
romarionelson <nelsonromario@hotmail.com>
2017-01-25 15:26:51 -0500

    report

--- reportqueries.php
+++ reportqueries.php
@@ -33,7 +33,9 @@ if($stmt_classification = $conn->prepare("Select Classification_Number from Clas
 //Now we have the list of Classification and Nielsen Category
 //  $query .= "WHERE " . implode (' AND ', $conditions);
 
-//$array1 = implode(',', array_fill(0, count($list_of_classification), '?'));
+$array1 = implode(',', array_fill(0, count($list_of_classification), '?'));
+
+$num_s = str_repeat('s',count($list_of_classification));
 echo "$list_of_classification";
 $first_query=<<<EOQA
  select count(distinct P.ProductID), SUM(Sales.Kilo_Vol)
@@ -42,7 +44,7 @@ $first_query=<<<EOQA
  LEFT JOIN Classification C  ON PC.ClassificationID = C.ClassificationID
  INNER JOIN Sales ON P.ProductID = Sales.ProductIDS
  WHERE Sales.Nielsen_Category = ? 
- and C.Classification_Number IN ($list_of_classification)
+ and C.Classification_Number IN ($array1)
 
 EOQA;
 
@@ -86,7 +88,7 @@ foreach ($list_of_nielsen_category as $value) {
 
 
 
-									$stmt_first->bind_param("s", $value); 
+									$stmt_first->bind_param("s$num_s", $value,$list_of_classification); 
 								    $stmt_first->execute();
                                     $stmt_first->bind_result($number_of_product, $sum_kilo_vol);
                                     $stmt_first->fetch();
