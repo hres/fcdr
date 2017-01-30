8ad33380ea7f01d6607af6c5b55297a4a822cca3
romarionelson <nelsonromario@hotmail.com>
2017-01-30 14:43:11 -0500

    changed fill product to as prepared

--- fill_product.php
+++ fill_product.php
@@ -6,13 +6,20 @@ $PID = ($_GET['ProductID']?$_GET['ProductID']:'');
 
 
 
-	
-	
-	$query = "  Select * from $dbname.Product P LEFT JOIN Product_Classification PC ON P.ProductID = PC.ProductID LEFT JOIN Classification C ON PC.ClassificationID = C.ClassificationID  where P.ProductID = $PID;
-";
-	$result = mysqli_query($conn,$query);
-						
+	$fill_product=<<<EOQ
+		Select * Product P LEFT JOIN Product_Classification PC
+		ON P.ProductID = PC.ProductID LEFT JOIN Classification
+		C ON PC.ClassificationID = C.ClassificationID  where P.ProductID = ?
+
+
+EOQ;
+
+							$stmt = $conn->prepare($fill_product);
+					     	$stmt->bind_param("i",$PID);
+							$result1 = $stmt->execute();	
+							$result = $stmt->get_result();											
 							
+						
 							
 
 if (!$result) {
