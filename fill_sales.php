b01f2aabcfbc415c96d8223e1aab62ce56f6c4a4
romarionelson <nelsonromario@hotmail.com>
2017-01-30 14:30:13 -0500

    changed delete product & delete sales to prepared

--- fill_sales.php
+++ fill_sales.php
@@ -5,13 +5,24 @@
 $PID = ($_GET['SalesID']?$_GET['SalesID']:'');
 
 
-	$query = "  SELECT * FROM $dbname.Sales P where P.SalesID = $PID";
-	$result = mysqli_query($conn,$query);
-						
+	//$query = "  SELECT * FROM $dbname.Sales P where P.SalesID = $PID";
+	//$result = mysqli_query($conn,$query);
+
+	$fill_sales =<<<EOQ
+		Select * from Sales
+		where SalesID = ?
+
+
+EOQ;
+
+							$stmt = $conn->prepare($fill_sales);
+					     	$stmt->bind_param("i",$PID);
+							$result1 = $stmt->execute();	
+							$result = $stmt->get_result();											
 							
 							
 
-if (!$result) {
+if (!$result1) {
     echo "ERRORS";
 }
 	
