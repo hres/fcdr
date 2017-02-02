fdba2f9e52d64d4e8992d548e33bbf1481c991c3
romarionelson <nelsonromario@hotmail.com>
2017-02-02 09:30:39 -0500

    report added

--- sample-design-report.php
+++ sample-design-report.php
@@ -362,23 +362,6 @@ $(document).ready(function() {
 	</script>
 
 
-		<script>
-
-$(document).ready(function() {
-
-$('.selectpicker').selectpicker({
-  style: 'btn-info',
-  size: 4
-});
-
-
-} );
-	</script>	
-	
-
-
-
-
 
 </body>
 </html>
