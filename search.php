eefcf1edf07035bf1d38086639f34a45c887a0fb
romarionelson <nelsonromario@hotmail.com>
2017-02-07 10:48:24 -0500

    search all

--- search.php
+++ search.php
@@ -451,7 +451,7 @@ if (!$result) {
 <?php include 'footer.php';?>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
-<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>
+<script type="text/javascript" src="js/jquery.validate.min.js"></script>
 
 	<script>
 	$(document).ready(function(){
