ef75ef1a305a23668a84409bc0f9deaade4b1e21
romarionelson <nelsonromario@hotmail.com>
2017-02-14 11:00:57 -0500

    list obj

--- sales_details.php
+++ sales_details.php
@@ -769,17 +769,7 @@ $(window).resize();
 </script>
 		</main>
 	<?php include 'footer.php';?>
-	<script type="text/javascript">
-				  var _gaq = _gaq || [];
-				  _gaq.push(['_setAccount', 'UA-365466-5']);
-				  _gaq.push(['_trackPageview']);
 
-				  (function() {
-					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-				  })();
-	</script>
 
 	<?php include 'List_Classification_Object.php'?>
 </body>
