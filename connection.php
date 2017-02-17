3c1a2ee5e1a025b6e13f7553e3e7d507c52d4144
romarionelson <nelsonromario@hotmail.com>
2017-02-17 09:53:27 -0500

    conn updated

--- connection.php
+++ connection.php
@@ -5,10 +5,10 @@ $db = parse_ini_file("conn.ini");
 /* now we can use the info in that file to create the appropriate, string connection, based on type, or just using the other info */
 
 
-$servername = "127.0.0.1";// $db['host']; 
-$username = "romario";//$db['user'];    
-$password = "romario";// $db['pass'];    
-$dbname = "fcdr";//$db['name'];     
+$servername =  $db['host']; 
+$username = $db['user'];    
+$password =  $db['pass'];    
+$dbname = $db['name'];     
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
