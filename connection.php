53d9748ae77bad5a1f01c9bb878cb8e2733f69d8
romarionelson <nelsonromario@hotmail.com>
2017-02-20 08:54:43 -0500

    commit

--- connection.php
+++ connection.php
@@ -6,12 +6,22 @@ $db = parse_ini_file("conn.ini");
 
 
 
+$servername ="127.0.0.1";//   $db['host']; //
+$username ="romario";//   $db['user'];   // 
+$password = "romario";//   $db['pass'];    //
+$dbname ="fcdr";//  $db['name'];//     
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+/*
+=======
 $servername = $db['host'];
 $username   = $db['user'];
 $password   = $db['pass'];
 $dbname     = $db['name'];
 $conn       = new mysqli($servername, $username, $password, $dbname);
+>>>>>>> 6659481b716a098c400d4691c6c81afd7d15fc83
 
+*/
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
---
+++ validate-nutrition.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+$("#vids-search-form1").validate({
+
+    rules: {
+
+        Dollar_Rank_To: {
+
+         required: {
+             
+             depends: function(element){
+          return $('#Dollar_Rank_From').val().length > 0;
+        }}
+
+},
+
+      Dollar_Rank_From : {
+    
+         required: {
+             
+             depends: function(element){
+          return $('#Dollar_Rank_To').val().length > 0;
+        }}
+
+},
+        date1: {
+      
+         required: {
+             
+             depends: function(element){
+          return $('#date2').val().length > 0;
+        }}
+
+},
+
+      date2 : {
+      
+         required: {
+             
+             depends: function(element){
+          return $('#date1').val().length > 0;
+        }}
+
+},
+        dateFrom: {
+      
+         required: {
+             
+             depends: function(element){
+          return $('#dateTo').val().length > 0;
+        }}
+
+},
+
+      dateTo : {
+      
+         required: {
+             
+             depends: function(element){
+          return $('#dateFrom').val().length > 0;
+        }}
+
+}
+        
+
+    },
+    messages: {
+        Dollar_Rank_From: "Fill in this value", 
+        Dollar_Rank_To: "Fill in this value",
+        dateTo: "", 
+        dateFrom: "", 
+        date1: "",
+        date2: ""
+    }
+
+
+});
+
+
+});
+
