c6d536cfe2ce0bf01e56b3d046c3681f6af5c69c
rsanchej <raul.sanchez@canada.ca>
2017-02-21 10:40:34 -0500

    date validation

--- create-market.php
+++ create-market.php
@@ -295,7 +295,7 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 <script type="text/javascript" src="js/jquery.validate.min.js"></script>
-<script type="text/javascript" src="js/validate-market.js"></script>
+<script type="text/javascript" src="validate-market.js"></script>
 
 	<script>
 	$(document).ready(function(){
