4ab2da813f5346814d8a5c2a7436f58574b4858f
rsanchej <raul.sanchez@canada.ca>
2017-02-21 11:37:49 -0500

    date validation

--- validate-market.js
+++ validate-market.js
@@ -1,24 +1,23 @@
 $.validator.addMethod(
 	"myDateFormat",
-    function(value, element) {
-        // yyyy/mm/dd
-        var re = /^\d{4}\/\d{1,2}\/\d{1,2}$/;
+	function(value, element) {
+		// yyyy/mm/dd
+		var re = /^\d{4}\/\d{1,2}\/\d{1,2}$/;
 
-        // valid if optional and empty OR if it passes the regex test
-        return (this.optional(element) && value == "") || re.test(value);
-    }
-);
+		// valid if optional and empty OR if it passes the regex test
+		return (this.optional(element) && value == "") || re.test(value);
+});
 
 $(document).ready(function() {
 	$("#vids-search-form1").validate({
 			rules: {
-				date1: {
+				Collection_Date: {
 					required: true,
 					date: true
 				}
 			},
 			message: {
-				date1: "wrong date format"
+				Collection_Date: "wrong date format"
 			}
 	});
 });
