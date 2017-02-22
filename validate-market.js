f44280a81301aab860a04848ae2199427732e6bf
romarionelson <nelsonromario@hotmail.com>
2017-02-22 10:26:08 -0500

    ms added

--- validate-market.js
+++ validate-market.js
@@ -11,8 +11,59 @@ $(document).ready(function() {
 				date1: {
 					required: true,
 					date: true
-				}
+				},
+				Dollar_Volume: {
+				required: true,
+				number: true
+				},
+				Kilo_Vol: {
+				required: true,
+				number: true
+				},
+				Dollar_Rank: {
+				required: true,
+				number: true
+				},
+				Dollar_Volume_PerCentage_Change: {
+				required: true,
+				number: true
+				},
+				Kilo_Volume_Percent_Change: {
+				required: true,
+				number: true
+				},
+				Average_AC_Dist: {
+				number: true
+				},
+				Dollar_Volume_Total: {
+				required: true,
+				number: true
+				},
+				Kilo_Volume_Total: {
+				required: true,
+				number: true
+				},
+				Dollar_Share: {
+				required: true,
+				number: true
+				},
+				Kilo_Share: {
+				required: true,
+				number: true
+				},
+				Cluster_Number: {
+				number: true
+				},
+				Kilo_Rank: {
+				number: true
+				},
+				Sales_UPC: {
+				required: true,
+				digits: true,
+				minlength:10
+				},
 			},
+			
 			messages: {
 				Collection_Date: "Required Date Format: YYYY/MM/DD"
 			}
