b6fa1d51b05ee05f832285045b52c94a36654e87
romarionelson <nelsonromario@hotmail.com>
2017-02-28 12:49:02 -0500

    bs message

--- edit_sales.php
+++ edit_sales.php
@@ -105,7 +105,7 @@
 	<div class="container">
 		<section style="margin-top: 15px;" class="hidethis">
 			<h3>Market Share</h3>
-			<div id="confirm-message" class="alert alert-success alert-dismissable fade in" style="display:none; margin-right:18px;" >
+			<div id="confirm-message" class="alert alert-success alert-dismissable" style="display:none; margin-right:18px;" >
 			   <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
 			
 			</div>
