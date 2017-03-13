366a03a4a51edf27e4e1715684ec682369bc532f
rsanchej <raul.sanchez@canada.ca>
2017-03-13 10:17:58 -0400

    added utf-8 header

--- session.php
+++ session.php
@@ -1,10 +1,9 @@
-<?php 
-
-			
-			session_start();
-		if(!isset($_SESSION['currentuser'])) {
-			header("Location: login.php");
-			die();
-		//echo "Your session is running " . $_SESSION['userName'];
-		}
-			?>
+<?php
+session_start();
+header('Content-Type: text/html; charset=utf-8');
+if (!isset($_SESSION['currentuser'])) {
+	header("Location: login.php");
+	die();
+	//echo "Your session is running " . $_SESSION['userName'];
+}
+?>
