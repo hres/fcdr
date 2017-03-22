b36d90bf462cfc0657b975584ab80f596090c8b9
rsanchej <raul.sanchez@canada.ca>
2017-03-22 15:00:00 -0400

    added custom error

--- edit_product.php
+++ edit_product.php
@@ -104,26 +104,16 @@
 		<div class="container" >
 
 <?php
-if (false) {
+if (count($sanitaion_errors) != 0) {
+	foreach ($error in $sanitaion_errors) {
 ?>
 
-<div class="alert alert-success">
-  <strong>Success!</strong> Indicates a successful or positive action.
-</div>
-
-<div class="alert alert-info">
-  <strong>Info!</strong> Indicates a neutral informative change or action.
-</div>
-
-<div class="alert alert-warning">
-  <strong>Warning!</strong> Indicates a warning that might need attention.
-</div>
-
 <div class="alert alert-danger">
-  <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
+<strong>Error!</strong> Please correct the following field: <?php echo $error;?>
 </div>
 
 <?php
+	}
 }
 ?>
 
