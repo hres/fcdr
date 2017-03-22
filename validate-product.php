bb27a27db9b0d07291f86fcb071eaf9527c4fa83
rsanchej <raul.sanchez@canada.ca>
2017-03-22 10:58:39 -0400

    added sample error messages

--- validate-product.php
+++ validate-product.php
@@ -29,22 +29,6 @@ if (isset($_POST['search'])) {
 
 }
 
-<div class="alert alert-success">
-  <strong>Success!</strong> Indicates a successful or positive action.
-</div>
-
-<div class="alert alert-info">
-  <strong>Info!</strong> Indicates a neutral informative change or action.
-</div>
-
-<div class="alert alert-warning">
-  <strong>Warning!</strong> Indicates a warning that might need attention.
-</div>
-
-<div class="alert alert-danger">
-  <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
-</div>
-
 /*
 
 FILTER_SANITIZE_NUMBER_FLOAT : CNF_CODE
