73e404733f4d3ffd9bf0e2130b393d21cbef3455
rsanchej <raul.sanchez@canada.ca>
2017-03-22 14:40:54 -0400

    adding sample error messages

--- edit_product.php
+++ edit_product.php
@@ -104,23 +104,27 @@
 		<div class="container" >
 
 <?php
+if (false) {
+?>
 
-// <div class="alert alert-success">
-  // <strong>Success!</strong> Indicates a successful or positive action.
-// </div>
+<div class="alert alert-success">
+  <strong>Success!</strong> Indicates a successful or positive action.
+</div>
 
-// <div class="alert alert-info">
-  // <strong>Info!</strong> Indicates a neutral informative change or action.
-// </div>
+<div class="alert alert-info">
+  <strong>Info!</strong> Indicates a neutral informative change or action.
+</div>
 
-// <div class="alert alert-warning">
-  // <strong>Warning!</strong> Indicates a warning that might need attention.
-// </div>
+<div class="alert alert-warning">
+  <strong>Warning!</strong> Indicates a warning that might need attention.
+</div>
 
-// <div class="alert alert-danger">
-  // <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
-// </div>
+<div class="alert alert-danger">
+  <strong>Danger!</strong> Indicates a dangerous or potentially negative action.
+</div>
 
+<?php
+}
 ?>
 
 <section style="margin-top: 15px;" class="hidethis">
