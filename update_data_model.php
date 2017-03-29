6b812f59cda68d0244041f779102799fbcc3598c
romarionelson <nelsonromario@hotmail.com>
2017-03-29 14:54:40 -0400

    OYESSSO

--- update_data_model.php
+++ update_data_model.php
@@ -7,17 +7,13 @@
 <?php
 
 
-echo "<h1>add classification</h1>";
+echo "<h1>TYPE CHANGE  classification</h1>";
 
-$query = "ALTER TABLE Package Add column Classification_Number FLOAT(6,6)";
+$query = "  ALTER TABLE Package modify Classification_Number varchar (9)";
 $result = mysqli_query($conn, $query);
 if($result){
-    echo "CN added";
-$query2 = "ALTER TABLE Package Add column Classification_Name TEXT";
-$result2 = mysqli_query($conn, $query2);
-if($result2){
-    echo "Both were created";
-}
+    echo "Type has been changed";
+
 }else{
     echo "FAILED";
 }
