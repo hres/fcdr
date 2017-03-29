fa8c95d94fa0cabc2f91de20cc8c4351c811c82b
romarionelson <nelsonromario@hotmail.com>
2017-03-29 10:49:38 -0400

    creating Classification

---
+++ update_data_model.php
@@ -0,0 +1,19 @@
+<?php
+
+include "connection.php";
+echo "<h1>add classification</h1>"
+
+$query = "ALTER TABLE Package Add column Classification_Number FLOAT('4','6')";
+$result = mysqli_query($conn, $query);
+if($result){
+    echo "CN added";
+$query2 = "ALTER TABLE Package Add column Classification_Name TEXT";
+$result2 = mysqli_query($conn, $query2);
+if($result2){
+    echo "Both were created";
+}
+}
+
+
+
+?>
