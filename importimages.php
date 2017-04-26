d4acb5e0029c233971932cd1576f891db30f60af
romarionelson <nelsonromario@hotmail.com>
2017-04-26 14:09:10 -0400

    integrety

--- importimages.php
+++ importimages.php
@@ -177,8 +177,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	<?php include 'footer.php';?>
 
 
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
 <script>
 
 $('#my-file-selector').on('change', function() {
