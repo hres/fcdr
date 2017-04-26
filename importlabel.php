3ebb635d7348ed53c603de4641c76141faede591
romarionelson <nelsonromario@hotmail.com>
2017-04-26 14:11:44 -0400

    commited

--- importlabel.php
+++ importlabel.php
@@ -201,8 +201,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 				  })();
 	</script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
 	<script>
 	$(document).ready(function(){
