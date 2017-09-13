82af5d0bab65d143d3e19c9ffdf0a652707a864e
romarionelson <nelsonromario@hotmail.com>
2017-09-13 10:15:03 -0400

    connection added

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$db = parse_ini_file("conn.ini");
+
+=======
+$servername = $db['host'];
+$username   = $db['user'];
+$password   = $db['pass'];
+$dbname     = $db['name'];
+$conn       = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
