f2f22a00ff8012690ce9a5ba94e7742380e17347
romarionelson <nelsonromario@hotmail.com>
2017-09-22 09:55:30 -0400

    added button

---
+++ cleardb.php
@@ -0,0 +1,33 @@
+<?php include 'connection.php';?>
+<?php
+
+
+ 
+
+	$query2 =  "Delete from $dbname.Sales where SalesID > 0"; 	
+	$result = mysqli_query($conn,$query2);	
+	
+
+	
+		$query3 =  "Delete from $dbname.Product_Component where ComponentID > 0"; 	
+		$result3 = mysqli_query($conn,$query3);	
+	
+			$query4 =  "Delete from $dbname.Images where ImageID > 0"; 	
+		$result4 = mysqli_query($conn,$query4);	
+	
+				$query5 =  "Delete from $dbname.Package where PackageID > 0"; 	
+		$result5 = mysqli_query($conn,$query5);	
+		
+					$query6 =  "Delete from $dbname.Product_Classification where ProductID > 0"; 	
+	$result6 = mysqli_query($conn,$query6);	
+	
+	
+		$query =  "Delete from $dbname.Product where ProductID > 0"; 	
+		$result7 = mysqli_query($conn,$query);	
+	echo "Database Cleared out";
+
+							
+
+
+$conn->close();
+?>
