5854b3783f2659e1fc2c8434dae4f88629b66ad3
romarionelson <nelsonromario@hotmail.com>
2017-09-22 10:20:39 -0400

    commit

--- validate-nutrient-analysis-report.php
+++
@@ -1,8 +0,0 @@
-<?php
-        	if (!empty($_POST["Cluster_Number"])  &&!filter_var($_POST["Cluster_Number"], FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION)) {
-		$sanitation_errors[] = "Cluster Number";
-	}
-    if (!empty($_POST["Sales_Year"])  && filter_var($_POST["Sales_Year"], FILTER_VALIDATE_INT)===false ) {
-		$sanitation_errors[] = "Sales Year";
-	}
-?>
