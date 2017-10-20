9056df53c46bcc3382e6b2bfc4a4306ed871f29a
romarionelson <nelsonromario@hotmail.com>
2017-10-20 14:35:11 -0400

    Update save_session.php

--- save_session.php
+++ save_session.php
@@ -5,18 +5,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 
 
-	$username = $_POST['uname'];
-	$password = $_POST['psw'];
 
+	$username = $_POST['uname'];
+	$hashedPassword = $_POST['psw']; 
 
 		$get_user =<<<EOQ
 		Select * from Users
-		where Username = ? AND Password = ?
+		where Username = ? 
 
 EOQ;
 
 							$stmt = $conn->prepare($get_user);
-					     	$stmt->bind_param("ss",$username,$password);
+					     	$stmt->bind_param("s",$username);
 							$result1 = $stmt->execute();	
 							$result = $stmt->get_result();
 	
@@ -25,19 +25,25 @@ EOQ;
 	$row = $result->fetch_assoc();	
 	if(mysqli_num_rows($result)>0){
 		
+	
+
+		$password = $row['Password'];
+
+		if(password_verify($hashedPassword,$password)){
+			
 		$username = $row['Name'];
 		$_SESSION['currentuser'] = $username;
-		
 
+		}else{
+$sanitation_errors[] = "Error";
+
+		}
 
 	}else{
 	
 	
 		$sanitation_errors[] = "Error";
-		//$tableau["errorPassword"] = "<div class=\"error\" style=\"color:#FF0000;\">Wrong username and/or password</div>";
-		
-	
-		
+
 	}
 	
 	
