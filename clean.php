755f1bf34f2b8f8b43e3da8d90f5d9bc7fdbe34f
romarionelson <nelsonromario@hotmail.com>
2018-01-17 10:08:31 -0500

    clean label table

---
+++ clean.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+<?php
+include "connection.php";
+
+$query = "truncate Product_Component";
+if($conn->query($query)){
+    echo "tructated table";
+
+
+    if($conn->query("delete from Package")){
+        echo "package deleted";
+    }else{
+        echo "package table not deleted";
+    }
+}else{
+    echo "failed truncating";
+}
+
+
+?>
+</body>
+
+</html>
